e8a36cfa1ee1f3d20f1e147168bd512197fc7ffc
ht0m <hartht@gmail.com>
2022-06-13 23:13:56 +0200

    Initial commit

---
+++ src/main/java/berthbooking/BerthbookingApplication.java
@@ -0,0 +1,13 @@
+package berthbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerthbookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BerthbookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/berthbooking/BerthbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package berthbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerthbookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
